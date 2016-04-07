88febce7d022855c715249afaa258f12f878f143
deadking01 <wuqingfeng234@163.com>
2016-04-07 23:50:36 +0800

    javascript dom pragram art

---
+++ practise/js/showPic.js
@@ -0,0 +1,64 @@
+function addLoadEvent(func){
+	var oldLoad = window.onload;
+	if(typeof window.onload!='function'){
+		window.onload=func;
+	}else{
+		window.onload=function(){
+			oldLoad();
+			func();
+		}
+	}
+}
+function insertAfter(newElelment,targetElement){
+	var parent =targetElement.parentNode;
+	if(parentNode.lastChild ==targetElement){
+		parent.appentChild(newElelment);
+	}else{
+		parent.insertBefore(newElelment,targetElement.nextSibling);
+	}
+}
+function preparePlaceholder(){
+	if(!document.createElement) return false;
+	if(!document.createTextNode) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery")) return false;
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/placeholder.gif");
+	placeholder.setAttribute("alt","my image gallery");
+
+	var description=document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext = document.createTextNode("choose an image");
+	description.appentChild(desctext);
+	var gallery=document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
+function prepareGallery(){
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery")) return false;
+	var links=gallery.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++){
+	  links[i].onclick=function(){
+	  	return showPic(this);
+	  }
+	  links[i].onkeypress=links[i].onclick;
+	}
+}
+function showPic(whichpic){
+	if(!document.getElementById("placeholder")) return true;
+	var source = whichpic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	if(!document.getElementById("description")) return false;
+	if(whichpic.getAttribute("title")){
+		var text=whichpic.getAttribute("title");
+	}else{
+		var text="";
+	}
+	return false;
+}
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
