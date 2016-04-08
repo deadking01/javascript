01767e00a2effb3c350b557306de31dcbe80e79a
deadking01 <wuqingfeng234@163.com>
2016-04-08 21:29:57 +0800

    debug project

--- practise/scripts/showPic.js
+++ practise/scripts/showPic.js
@@ -39,6 +39,7 @@ function prepareGallery(){
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
 	if(!document.getElementById("imagegallery")) return false;
+	var gallery=document.getElementById("imagegallery");
 	var links=gallery.getElementsByTagName("a");
 	for(var i=0;i<links.length;i++){
 	  links[i].onclick=function(){
