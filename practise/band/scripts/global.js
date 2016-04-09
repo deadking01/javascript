7b2a219987f45536dc7519bc2f41116a1e5d2db8
deadking01 <wuqingfeng234@163.com>
2016-04-09 21:14:48 +0800

    debug

--- practise/band/scripts/global.js
+++ practise/band/scripts/global.js
@@ -30,7 +30,7 @@ function addClass(element,value){
 function highlightPage(){
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
-	var headers=Document.getElementsByTagName('header');
+	var headers=document.getElementsByTagName('header');
 	if(headers.length ==0) return false;
 	var navs=headers[0].getElementsByTagName('nav');
 	if(navs.length==0) return false;
