5ab9205ccd7615adcddefdf4f36919edcc68db2a
deadking01 <deadking01@users.noreply.github.com>
2016-04-11 18:27:15 +0800

    Update global.js

--- practise/band/scripts/global.js
+++ practise/band/scripts/global.js
@@ -129,5 +129,149 @@ function prepareSlideshow(){
 	}
 
 }
+function showSelection(id){
+	var sections = document.getElementsByTagName("section");
+	for(var i=0;i<sections.length;i++){
+		if(sections[i].getAttribute("id")!=id){
+			sections[i].style.display="none";
+		}else{
+			sections[i].style.display="block";
+		}
+	}
+}
+function prepareInternalnav(){
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	var articles=document.getElementsByTagName("article");
+	if(articles.length==0) return false;
+	var navs=articles[0].getElementsByTagName("nav");
+	if(navs.length==0) return false;
+	var nav=navs[0];
+	var links=nav.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++){
+		var sectionId=links[i].getAttribute("href").split("#")[1];
+		if(!document.getElementById(sectionId)) continue;
+		document.getElementById(sectionId).style.display="none";
+		links[i].destination=sectionId;
+		links[i].onclick=function(){
+			showSelection(this.destination);
+			return false;
+		}
+	}
+}
+function preparePlaceholder(){
+	if(!document.createElement) return false;
+	if(!document.createTextNode) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery")) return false;
+	var placeholder = document.createElement("img");
+	placeholder.setAttribute("id","placeholder");
+	placeholder.setAttribute("src","images/placeholder.gif");
+	placeholder.setAttribute("alt","my image gallery");
+
+	var description=document.createElement("p");
+	description.setAttribute("id","description");
+	var desctext = document.createTextNode("choose an image");
+	description.appendChild(desctext);
+	var gallery=document.getElementById("imagegallery");
+	insertAfter(placeholder,gallery);
+	insertAfter(description,placeholder);
+}
+function prepareGallery(){
+	if(!document.getElementsByTagName) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("imagegallery")) return false;
+	var gallery=document.getElementById("imagegallery");
+	var links=gallery.getElementsByTagName("a");
+	for(var i=0;i<links.length;i++){
+	  links[i].onclick=function(){
+	  	return showPic(this);
+	  }
+	  links[i].onkeypress=links[i].onclick;
+	}
+}
+function showPic(whichpic){
+	if(!document.getElementById("placeholder")) return true;
+	var source = whichpic.getAttribute("href");
+	var placeholder = document.getElementById("placeholder");
+	placeholder.setAttribute("src",source);
+	if(!document.getElementById("description")) return false;
+	if(whichpic.getAttribute("title")){
+		var text=whichpic.getAttribute("title");
+	}else{
+		var text="";
+	}
+	return false;
+}
+function stripeTables(){
+	if(!document.getElementsByTagName) return false;
+	var tables=document.getElementsByTagName("table");
+	for(var i=0;i<tables.length;i++){
+		var odd=false;
+		var rows=tables[i].getElementsByTagName("tr");
+		for(var j=0;j<rows.length;j++){
+			if(odd=true){
+				addClass(rows[j],"odd");
+				odd=false;
+			}else{
+				odd=true;
+			}
+		}
+	}
+}
+function hightLightRows(){
+	if(!document.getElementsByTagName) return false;
+	var rows=document.getElementsByTagName("tr");
+	for(var i=0;i<rows.length;i++){
+		rows[i].oldClassName=rows[i].className;
+		rows[i].onmouseeover=function(){
+			addClass(this,"highlight");
+		}
+		rows[i].onmouseout=function(){
+			this.className=this.oldClassName;
+		}
+	}
+}
+function displayAbbreviations(){
+	if(!document.getElementsByTagName||!document.createElement||!document.createTextNode) return false;
+	var abbreviations=document.getElementsByTagName("abbr");
+	if(abbreviations.length<1) return false;
+	var defs=new Array();
+	for(var i=0;i<abbreviations.length;i++){
+		var current_abbr=abbreviations[i];
+		if(current_abbr.childNodes.length<1) continue;
+		var definition=current_abbr.getAttribute("title");
+		var key=current_abbr.lastChild.nodeValue;
+		defs[key]=definition;
+	}
+	var dlist=document.createElement("dl");
+	for(key in defs){
+		var definition=defs[key];
+		var dtitle=document.createElement("dt");
+		var dtitile_text=document.createTextNode(key);
+		dtitle.appendChild(dtitile_text);
+		var ddesc=document.createElement("dd");
+		var ddesc_text=document.createTextNode(definition);
+		ddesc.appendChild(ddesc_text);
+		dlist.appendChild(dtitle);
+		dlist.appendChild(ddesc);
+	}
+	if(dlist.childNodes.length<1) return false;
+	var header=document.createElement("h3");
+	var header_text=document.createTextNode("Abbreviations");
+	header.appendChild(header_text);
+	var articles=document.getElementsByTagName("article");
+	if(articles.length==0) return false;
+	var container=articles[0];
+	container.appendChild(header);
+	container.appendChild(dlist);
+
+}
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
+addLoadEvent(prepareInternalnav);
+addLoadEvent(preparePlaceholder);
+addLoadEvent(prepareGallery);
+addLoadEvent(hightLightRows);
+addLoadEvent(displayAbbreviations);
+addLoadEvent(stripeTables);
